2350d06d66b7b584a298fb541b1bb15f1c5c45d5
jfeddern <jan.feddern@gmx.net>
2020-12-27 22:44:05 +0100

    Add support for rewrite plugin in node-cache

--- cmd/node-cache/main.go
+++ cmd/node-cache/main.go
@@ -29,6 +29,7 @@ import (
 	_ "github.com/coredns/coredns/plugin/metrics"
 	_ "github.com/coredns/coredns/plugin/pprof"
 	_ "github.com/coredns/coredns/plugin/reload"
+	_ "github.com/coredns/coredns/plugin/rewrite"
 	_ "github.com/coredns/coredns/plugin/template"
 	_ "github.com/coredns/coredns/plugin/whoami"
 	"k8s.io/dns/pkg/version"
