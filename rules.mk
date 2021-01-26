8448480a3c311200e8dfbb6ea142fc4ebf8bf899
pacoxu <paco.xu@daocloud.io>
2021-01-26 15:14:26 +0800

    update debian-iptables to buster-v1.4.0 for base change debian-base 1.3.0
    Signed-off-by: pacoxu <paco.xu@daocloud.io>

--- rules.mk
+++ rules.mk
@@ -30,7 +30,7 @@ SRC_DIRS := cmd pkg
 
 ALL_ARCH := amd64 arm arm64 ppc64le s390x
 BASEIMAGE ?= k8s.gcr.io/build-image/debian-base-$(ARCH):buster-v1.3.0
-IPTIMAGE ?= k8s.gcr.io/build-image/debian-iptables-$(ARCH):buster-v1.3.0
+IPTIMAGE ?= k8s.gcr.io/build-image/debian-iptables-$(ARCH):buster-v1.4.0
 
 # These rules MUST be expanded at reference time (hence '=') as BINARY
 # is dynamically scoped.
